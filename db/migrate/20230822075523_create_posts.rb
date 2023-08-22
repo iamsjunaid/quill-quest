4260d123166815803085f1857214b92b6416a44d
Junaid Syed <sjunaid626@gmail.com>
2023-08-22 13:49:20 +0530

    Update foreign key references

--- db/migrate/20230822075523_create_posts.rb
+++ db/migrate/20230822075523_create_posts.rb
@@ -1,10 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
+    t.references :author_id, foreign_key: { to_table: :users }
     t.string :title
     t.text :text
-    t.integer :likes_counter
-    t.integer :comments_counter
-
+    t.integer :comments_counter, default: 0
+    t.integer :likes_counter, default: 0
+    
     t.timestamps
   end
 end
