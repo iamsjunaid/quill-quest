86916b6155fbf1b830252ff9f334961c36333923
Junaid Syed <sjunaid626@gmail.com>
2023-08-23 14:54:36 +0530

    establish associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
-class Post < ApplicationRecord
+class User < ApplicationRecord
+  has_many :comments, class : "Comment", foreign_key: "author_id"
+  has_many :posts, class : "Post", foreign_key: "author_id"
+  has_many :likes, class : "Like", foreign_key: "author_id"
 end
