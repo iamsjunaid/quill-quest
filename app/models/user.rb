1da8b42e7c365321ca32612414838ed39ed62230
Junaid Syed <sjunaid626@gmail.com>
2023-08-23 15:00:27 +0530

    Remove syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :comments, class : "Comment", foreign_key: "author_id"
-  has_many :posts, class : "Post", foreign_key: "author_id"
-  has_many :likes, class : "Like", foreign_key: "author_id"
+  has_many :comments, class: "Comment", foreign_key: "author_id"
+  has_many :posts, class: "Post", foreign_key: "author_id"
+  has_many :likes, class: "Like", foreign_key: "author_id"
 end
