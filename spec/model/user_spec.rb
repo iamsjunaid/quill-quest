f04bd30506d8ab4ab2d1f7e9b2bd634be28f7c4c
Junaid Syed <sjunaid626@gmail.com>
2023-08-24 11:21:11 +0530

    add test: name must not be blank

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   subject{ User.new(name: "Example User", photo:"https://example.com/photo.jpg", bio:"Example bio", posts_counter: 0) }
   
   before{ subject.save }
-
-  end
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
 end
