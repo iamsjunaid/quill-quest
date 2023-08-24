14d6688aaa371f3b0b605f54b796d1b718ba90eb
Junaid Syed <sjunaid626@gmail.com>
2023-08-24 11:53:51 +0530

    add context for tests

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject{ User.new(name: "John", photo:"https://example.com/photo.jpg", bio:"Example bio", posts_counter: 5) }
-  
-  before{ subject.save }
-    it "is valid with valid name" do
-      expect(subject).to be_valid
+  subject { User.new(name: 'John', photo: 'https://example.com/photo.jpg', bio: 'Example bio', posts_counter: 5) }
+
+  before { subject.save }
+  describe '#name' do
+    context 'when name is nil' do
+      it 'returns not valid without a name' do
+        subject.name = nil
+        expect(subject).to_not be_valid
+      end
     end
 
-    it "is not valid without a name" do
-      subject.name = nil
-      expect(subject).to_not be_valid
+    context 'when name is empty' do
+      it 'returns not valid without a name' do
+        subject.name = ''
+        expect(subject).to_not be_valid
+      end
     end
+
+    context 'when name is valid' do
+      it 'returns valid' do
+        expect(subject).to be_valid
+      end
+    end
+  end
 end
