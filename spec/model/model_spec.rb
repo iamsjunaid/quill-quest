fa4999278c573be004be62b7cce700531bd19053
Junaid Syed <sjunaid626@gmail.com>
2023-08-24 14:36:05 +0530

    fix linter errors

--- spec/model/model_spec.rb
+++ spec/model/model_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Post, type: :model do
     context 'when title is nil' do
       it 'returns false without a title' do
         author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-        post = Post.new(title: 'Example title', text: 'Example body', author: author)
+        post = Post.new(title: 'Example title', text: 'Example body', author:)
 
         post.title = nil
         expect(post).to_not be_valid
@@ -15,7 +15,7 @@ RSpec.describe Post, type: :model do
     context 'when title is valid' do
       it 'return false when title is too long' do
         author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-        post = Post.new(title: 'Example title', text: 'Example body', author: author)
+        post = Post.new(title: 'Example title', text: 'Example body', author:)
 
         post.title = 'a' * 251
         expect(post).to_not be_valid
@@ -23,7 +23,7 @@ RSpec.describe Post, type: :model do
 
       it 'returns true' do
         author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-        post = Post.new(title: 'Example title', text: 'Example body', author: author)
+        post = Post.new(title: 'Example title', text: 'Example body', author:)
 
         expect(post).to be_valid
       end
@@ -33,17 +33,34 @@ RSpec.describe Post, type: :model do
   describe '#comments_counter' do
     it 'is valid with an integer comments_counter' do
       author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-      post = Post.new(title: 'Example title', text: 'Example body', comments_counter: 5, author: author)
+      post = Post.new(title: 'Example title', text: 'Example body', comments_counter: 5, author:)
 
       expect(post).to be_valid
     end
 
     it 'is invalid with a non-integer comments_counter' do
       author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-      post = Post.new(title: 'Example title', text: 'Example body', comments_counter: 'not_an_integer', author: author)
+      post = Post.new(title: 'Example title', text: 'Example body', comments_counter: 'not_an_integer', author:)
 
       expect(post).to_not be_valid
-      expect(post.errors[:comments_counter]).to include("is not a number")
+      expect(post.errors[:comments_counter]).to include('is not a number')
+    end
+  end
+
+  describe '#likes_counter' do
+    it 'is valid with an integer likes_counter' do
+      author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
+      post = Post.new(title: 'Example title', text: 'Example body', likes_counter: 5, author:)
+
+      expect(post).to be_valid
+    end
+
+    it 'is invalid with a non-integer likes_counter' do
+      author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
+      post = Post.new(title: 'Example title', text: 'Example body', likes_counter: 'not_an_integer', author:)
+
+      expect(post).to_not be_valid
+      expect(post.errors[:likes_counter]).to include('is not a number')
     end
   end
 end
