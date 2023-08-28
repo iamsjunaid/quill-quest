7726c459d1cbeace4663081e48ef4eb7868c5767
Junaid Syed <sjunaid626@gmail.com>
2023-08-28 17:11:52 +0530

    Create tests for users_controller: test show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,4 +25,26 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include("Here is a list of all the users")
     end
   end
+
+  
+  describe "GET #show" do
+    before(:example) { get user_path(subject) } # get(:show)
+    it "returns http success" do
+      # get "/users/#{subject.id}"
+      # get user_path(subject)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the correct template" do
+      # get "/users/#{subject.id}"
+      # get user_path(subject)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      # get "/users/#{subject.id}"
+      # get user_path(subject)
+      expect(response.body).to include("Here is the given user")
+    end
+  end
 end
