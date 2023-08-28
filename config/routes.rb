5f3ad43a208697a587f5b56412039c5b7240c11d
Junaid Syed <sjunaid626@gmail.com>
2023-08-28 11:44:53 +0530

    Update resources :user and add home root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  root 'users#index', as: 'home'
 end
