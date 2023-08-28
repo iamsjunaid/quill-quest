b22a64b1b6c203727625c0c39d3303d1ba4633a1
Junaid Syed <sjunaid626@gmail.com>
2023-08-28 17:56:04 +0530

    Create tests for posts_controller: test index

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'this is a bio',
+                     posts_counter: 12)
+
+  subject do
+    Post.new(title: 'this is a title', text: 'this is text', author: user)
+  end
+
+  before { subject.save }
+
+  describe 'GET #index' do
+    before(:example) { get user_posts_path(user_id: 1) } # get(:index)
+    it 'returns http success' do
+      # get '/posts'
+      # get posts_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct template' do
+      # get '/posts'
+      # get posts_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      # get '/posts'
+      # get posts_path
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
   end
 end
