59b4c4821ad883fcbe70c80557d902392f47d3d6
Junaid Syed <sjunaid626@gmail.com>
2023-08-28 17:10:49 +0530

    Create tests for users_controller: test index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  subject { User.new(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends', posts_counter: 12) }
+
+  before { subject.save } 
+
+  describe "GET #index" do
+    before(:example) { get users_path } # get(:index)
+    it "returns http success" do
+      # get '/users'
+      # get users_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the correct template" do
+      # get '/users'
+      # get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      # get '/users'
+      # get users_path
+      expect(response.body).to include("Here is a list of all the users")
+    end
   end
 end
