a53aa6dbb46ff43fbde817027c180aca984c8ddb
Junaid Syed <sjunaid626@gmail.com>
2023-08-28 17:56:42 +0530

    Create tests for posts_controller: test show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -30,4 +30,25 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
+
+  describe 'GET #show' do
+    before(:each) { get user_post_path(user_id: 1, id: 1) }
+    it 'returns http success' do
+      # get "/posts/#{subject.id}"
+      # get posts_path(subject)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct template' do
+      # get "/posts/#{subject.id}"
+      # get posts_path(subject)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      # get "/posts/#{subject.id}"
+      # get posts_path(subject)
+      expect(response.body).to include('Here is a post for a given user')
+    end
+  end
 end
