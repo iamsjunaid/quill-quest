3151eaf7c85710dd0c5f199f1232212ce3feee02
Junaid Syed <sjunaid626@gmail.com>
2023-09-04 14:45:06 +0530

    Refactor the tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,49 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  subject { User.new(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends', posts_counter: 12) }
-
+  subject { @user = User.create(name: 'Batman & Superman', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Superheroes', posts_counter: 10) }
   before { subject.save }
 
   describe 'GET #index' do
-    before(:example) { get users_path } # get(:index)
+    before(:example) { get users_path }
+    
     it 'returns http success' do
-      # get '/users'
-      # get users_path
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the correct template' do
-      # get '/users'
-      # get users_path
       expect(response).to render_template(:index)
     end
 
     it 'includes the correct placeholder text in the response body' do
-      # get '/users'
-      # get users_path
-      expect(response.body).to include('Here is a list of all the users')
+      expect(response.body).to include(@user.photo)
     end
   end
 
   describe 'GET #show' do
-    before(:example) { get user_path(subject) } # get(:show)
+    before(:example) { get user_path(id: @user.id) }
+    
     it 'returns http success' do
-      # get "/users/#{subject.id}"
-      # get user_path(subject)
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the correct template' do
-      # get "/users/#{subject.id}"
-      # get user_path(subject)
       expect(response).to render_template(:show)
     end
 
     it 'includes the correct placeholder text in the response body' do
-      # get "/users/#{subject.id}"
-      # get user_path(subject)
-      expect(response.body).to include('Here is the given user')
+      expect(response.body).to include(@user.photo)
     end
   end
 end
