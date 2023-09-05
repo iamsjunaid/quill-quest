b66076781d7ec0380040fb6f59e9fae9700d256f
Junaid Syed <sjunaid626@gmail.com>
2023-09-05 17:39:52 +0530

    update tests

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -1,39 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe 'Post Index', type: :system do
-  before :each do
-    @user = User.create(name: 'Batman', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Superhero',
-                        posts_counter: 6)
-    @post = Post.create(title: 'The Dark Night', text: 'This is my first post', comments_counter: 15,
-                        likes_counter: 18, author: @user)
-    @comment1 = Comment.create(author: @user, post: @post, text: 'This is my first comment')
-    @comment2 = Comment.create(author: @user, post: @post, text: 'This is my second comment')
-    @comment3 = Comment.create(author: @user, post: @post, text: 'This is my third comment')
-    @like = Like.create(author: @user, post: @post)
-  end
+  describe 'Post index page' do
+    before :each do
+      @user = User.create(name: 'Batman', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Superhero',
+                          posts_counter: 6)
+      @post = Post.create(title: 'The Dark Night', text: 'This is my first post', comments_counter: 15,
+                          likes_counter: 18, author: @user)
+      @comment1 = Comment.create(author: @user, post: @post, text: 'This is my first comment')
+      @comment2 = Comment.create(author: @user, post: @post, text: 'This is my second comment')
+      @comment3 = Comment.create(author: @user, post: @post, text: 'This is my third comment')
+      @like = Like.create(author: @user, post: @post)
+        
+      visit user_posts_path(user_id: @user.id)
+    end
 
-  scenario "see user's profile picture, username, number of posts and interactions" do
-    visit user_posts_path(user_id: @user.id)
-    expect(page).to have_content('Batman')
-    expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']")
-    expect(page).to have_content('Number of posts: 7')
-    expect(page).to have_content('Comments: 18')
-    expect(page).to have_content('Likes: 19')
-  end
+    it "shows user's profile picture" do
+      expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']")
+    end
 
-  scenario "see some of the post's title, body and first comments" do
-    visit user_posts_path(user_id: @user.id)
-    expect(page).to have_content('This is my first post')
-  end
+    it 'shows the users username' do
+      expect(page).to have_content('Batman')
+    end
 
-  scenario 'see a section for pagination if there are more posts than fit on the view' do
-    visit user_posts_path(user_id: @user.id)
-    expect(page).to have_content('Pagination')
-  end
+    it 'shows number of posts by user' do
+      expect(page).to have_content('Number of posts: 7')
+    end
 
-  scenario "clicking on a post, it redirects me to that post's show page" do
-    visit user_posts_path(user_id: @user.id)
-    click_link 'Post #'
-    expect(page).to have_current_path(user_post_path(@user, @post))
+    it 'shows posts title' do
+      expect(page).to have_content('Post #')
+    end
+
+    it 'shows some of the posts body' do
+      expect(page).to have_content 'This is my first post'
+    end
+
+    it 'shows the first comment on a post' do
+      expect(page).to have_content 'This is my first comment'
+    end
+
+    it 'shows how many comments a post has.' do
+      expect(page).to have_content('Comments: 1')
+    end
+
+    it 'shows how many likes a post has.' do
+      expect(page).to have_content('Likes: 1')
+    end
+
+    it 'shows a section for pagination if there are more posts than fit on the view' do
+      expect(page).to have_content('Pagination')
+    end
+
+    it 'when user clicks on a post, it redirects to that posts show page' do
+      click_link 'Post #'
+      expect(page).to have_current_path(user_post_path(@user, @post))
+    end
   end
 end
