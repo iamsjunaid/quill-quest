1e8933ce418016feadf5ed4ca6c8754aa72ada46
Junaid Syed <sjunaid626@gmail.com>
2023-09-05 12:40:36 +0530

    Update `posts_controller.rb`

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  
+  before_action :find_user, only: [:index]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :asc)
