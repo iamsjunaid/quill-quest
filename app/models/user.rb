94fc36c137dd5a7b1f5d46941ea067874c04c7ca
Junaid Syed <sjunaid626@gmail.com>
2023-09-06 13:44:49 +0530

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 
-  validates :name, presence: true, length: { in: 3..25 }
+  validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than: 0 }
 
   def recent_posts
