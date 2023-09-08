ac31227230101e3ab2e5f40dd5a8ff0d90723878
Junaid Syed <sjunaid626@gmail.com>
2023-09-08 13:47:22 +0530

    Add index method

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::PostsController < ApplicationController
   before_action :find_user
-    private
+  
+  def index
+    @posts = @user.posts
+    render json: @posts
+  end  
+  
+  private
 
-    def find_user
-      @user = User.find(params[:user_id])
-    end
+  def find_user
+    @user = User.find(params[:user_id])
+  end
 
 end
