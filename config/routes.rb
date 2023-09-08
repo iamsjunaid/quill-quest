65530a19bad3ee648de0c384dd54069ca5e06105
Junaid Syed <sjunaid626@gmail.com>
2023-09-08 13:50:57 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,14 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+        end
+      end
+    end
+  end
+  
   root 'users#index', as: 'home'
 end
